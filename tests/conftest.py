d4f89384c8a588bfcb4e45635a175c6abf9eea71
syed-dillu <syeddillu4k786@gmail.com>
2024-11-01 16:22:56 +0530

    Added text messages

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,16 @@ import pytest
 from sqlmodel import SQLModel
 from bookstore.database import engine
 
+
 @pytest.fixture(scope="session",autouse=True)
 def setup_database():
-    """ Drop the data and create the data """
+    """
+
+    Fixture to set up the database for testing.
+    This fixture runs once per test session. It drops all existing tables, creates new tables,
+    and provides a clean database state for tests. After all tests have run, it drops the tables again.
+
+    """    
     SQLModel.metadata.drop_all(bind=engine)
     SQLModel.metadata.create_all(bind=engine)
     yield
