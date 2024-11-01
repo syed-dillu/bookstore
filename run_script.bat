6c04e3732178994463e28a259e85b858f0e2845a
syed-dillu <syeddillu4k786@gmail.com>
2024-11-01 15:30:20 +0530

    change file name

--- run_script.bat
+++ run_script.bat
@@ -3,7 +3,7 @@ REM Run pytest
 pytest
 
 REM Open coverage html report
-start .\reports\coverage_report_html\index.html
+start .\reports\coverage_report\index.html
 
 REM Serve Allure report
 start cmd /k "allure serve .\reports\allure-results"
